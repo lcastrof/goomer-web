8181e0ce9b0b94db8962c0211d7e10716d984568
Lucas de Castro <castro.lucas@engenharia.ufjf.br>
2020-07-25 20:11:40 -0300

    connecting to api

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../../services/api';
 
 import { Container } from './styles';
 
 const Main: React.FC = () => {
-  return <Container>Main Page</Container>;
+  useEffect(() => {
+    async function loadRestaurants(): Promise<void> {
+      const { data } = await api.get('/');
+
+      console.log(data);
+    }
+
+    loadRestaurants();
+  }, []);
+
+  return <Container>Main page</Container>;
 };
 
 export default Main;
