a758025b7315050fd9e701740dda895bcceb1fc4
Lucas de Castro <castro.lucas@engenharia.ufjf.br>
2020-07-31 12:41:29 -0300

    static main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useEffect } from 'react';
 import api from '../../services/api';
 
-import { Container } from './styles';
+import {
+  Container,
+  WelcomeMessage,
+  SearchInput,
+  SearchIcon,
+  Cards,
+  RestaurantCard,
+  RestaurantInfo,
+} from './styles';
 
 const Main: React.FC = () => {
   useEffect(() => {
@@ -14,7 +23,150 @@ const Main: React.FC = () => {
     loadRestaurants();
   }, []);
 
-  return <Container>Main page</Container>;
+  return (
+    <Container>
+      <WelcomeMessage>Bem-vindo ao Lista Rango</WelcomeMessage>
+
+      <SearchInput>
+        <input type="text" placeholder="Buscar estabelecimento" />
+        <SearchIcon />
+      </SearchInput>
+
+      <Cards>
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+
+        <RestaurantCard to="/restaurant">
+          <img
+            src="https://cdn2.vectorstock.com/i/1000x1000/78/26/restaurant-logo-design-vector-10067826.jpg"
+            alt="restaurant image"
+          />
+          <RestaurantInfo>
+            <strong>Nome do Restaurante</strong>
+            <span>Endereco do restaurante</span>
+          </RestaurantInfo>
+        </RestaurantCard>
+      </Cards>
+    </Container>
+  );
 };
 
 export default Main;
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,3 +1,129 @@
 import styled from 'styled-components';
+import { FiSearch } from 'react-icons/fi';
 
-export const Container = styled.div``;
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  padding: 32px 75px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const WelcomeMessage = styled.div`
+  text-align: center;
+  font-size: 24px;
+
+  color: var(--gray);
+`;
+
+export const SearchInput = styled.div`
+  display: flex;
+  width: 840px;
+  height: 40px;
+  margin-top: 32px;
+
+  position: relative;
+
+  input {
+    width: 100%;
+    height: 100%;
+    background: var(--restaurant-search);
+    box-shadow: 0 2px 4px #00000029;
+    border-radius: 45px;
+    padding: 10px 39px;
+    color: var(--gray);
+
+    &::placeholder {
+      color: var(--gray);
+    }
+
+    ~ svg {
+      position: relative;
+      top: 12px;
+      left: -31px;
+
+      transition: 180ms ease-in-out;
+    }
+
+    &:focus {
+      border: 1px solid var(--primary);
+
+      ~ svg {
+        color: var(--primary);
+      }
+    }
+  }
+`;
+
+export const SearchIcon = styled(FiSearch)`
+  width: 16px;
+  height: 16px;
+
+  color: var(--gray);
+`;
+
+export const Cards = styled.div`
+  margin-top: 46px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+
+  flex-wrap: wrap;
+`;
+
+export const RestaurantCard = styled(Link)`
+  display: flex;
+  align-items: center;
+  width: 367px;
+  height: 100px;
+  box-shadow: 0 2px 4px #00000029;
+  position: relative;
+  margin-bottom: 52px;
+  margin-right: 48px;
+  text-decoration: none;
+
+  img {
+    width: 100px;
+    height: 100px;
+    margin-right: 25px;
+  }
+
+  &::after {
+    content: 'Aberto agora';
+    position: absolute;
+    right: -20px;
+    top: -20px;
+
+    width: 48px;
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    background: var(--secondary);
+    border-radius: 50%;
+
+    color: var(--white);
+    font-size: 8px;
+    font-weight: bold;
+  }
+`;
+
+export const RestaurantInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+
+  color: var(--gray);
+
+  span {
+    font-size: 12px;
+    font-weight: 400;
+  }
+`;
